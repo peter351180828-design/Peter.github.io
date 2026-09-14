5576773dc750db709dd07322b3b8951ba4b42901
peter351180828-design <peter351180828@gmail.com>
2026-09-14 10:02:35 +0800

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -4,8 +4,8 @@
  * Publishable key 本来就是给浏览器公开使用的；不要把 Secret key 填在这里。
  */
 window.PHOTO_ARCHIVE_CONFIG = {
-  supabaseUrl: 'PASTE_SUPABASE_PROJECT_URL_HERE',
-  supabaseKey: 'PASTE_SUPABASE_PUBLISHABLE_KEY_HERE',
+  supabaseUrl: 'https://kspatelbqtoyotxtadqn.supabase.co',
+  supabaseKey: 'sb_publishable_yrHi4_DDO10AE-sfp2Sd5Q_7voGT7Pa',
   siteName: '你的名字',
   siteSubtitle: '摄影档案',
   storageBucket: 'photos',
